b7ee229e616576e9ab966d13ff5b5505a7e148ea
Miller García Quimbaya <Miller.Garcia@interedes.local>
2020-10-15 16:51:07 -0500

    fix bug of show wrong emotion

--- main.js
+++ main.js
@@ -23,38 +23,40 @@ function main(){
         CVD.ctx.clearRect(0,0,CVD.canvas.width, CVD.canvas.height);
         CVD.ctx.strokeRect(faceCoo.x, faceCoo.y, faceCoo.w, faceCoo.h);
         CVD.update_canvasTexture();
+
+        if(detectState.expressions){
+          
+          CVD.ctx.font = "bold 22px sans-serif";
+          const expr = detectState.expressions;
+          const mouthOpen = expr[0];
+          const mouthSmile = expr[1];
+          const eyebrowFrown = expr[2];
+          const eyebrowRaised = expr[3];
+          if (mouthSmile > 0.6){
+            //console.log("feliz");         
+           
+            CVD.ctx.fillText("Feliz",faceCoo.x,faceCoo.y);
+          }
+          if(mouthOpen > 0.27 || eyebrowRaised >0.2){
+            //console.log("sorprendido");                                         
+            CVD.ctx.fillText("Sorprendido",faceCoo.x,faceCoo.y);
+            
+          }
+          if (eyebrowFrown>0.2){
+            //console.log("Triste")
+            
+            CVD.ctx.fillText("Triste",faceCoo.x,faceCoo.y);
+          }        
+          }else{
+            CVD.ctx.fillText("",0,0);
+          }
         
       }else{
         CVD.ctx.clearRect(0,0,CVD.canvas.width, CVD.canvas.height);
       }
       
       
-      if(detectState.expressions){
-        const faceCoo = CVD.getCoordinates(detectState);
-        CVD.ctx.font = "bold 22px sans-serif";
-        const expr = detectState.expressions;
-        const mouthOpen = expr[0];
-        const mouthSmile = expr[1];
-        const eyebrowFrown = expr[2];
-        const eyebrowRaised = expr[3];
-        if (mouthSmile > 0.6){
-          //console.log("feliz");         
-         
-          CVD.ctx.fillText("Feliz",faceCoo.x,faceCoo.y);
-        }
-        if(mouthOpen > 0.27 || eyebrowRaised >0.2){
-          //console.log("sorprendido");                                         
-          CVD.ctx.fillText("Sorprendido",faceCoo.x,faceCoo.y);
-          
-        }
-        if (eyebrowFrown>0.2){
-          //console.log("Triste")
-          
-          CVD.ctx.fillText("Triste",faceCoo.x,faceCoo.y);
-        }        
-        }else{
-          CVD.ctx.fillText("",0,0);
-        }
+      
       
       
       CVD.draw();
