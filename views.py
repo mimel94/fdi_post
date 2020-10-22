5ffa5a9c86f062d11c098200c3e1cbf333010532
Miller García Quimbaya <Miller.Garcia@interedes.local>
2020-10-22 10:00:33 -0500

    actual to server

--- views.py
+++ views.py
@@ -62,13 +62,13 @@ pnet, rnet, onet = detect_face.create_mtcnn(sess=facenet_persistent_session, mod
 
 rutaHarr=''
 global client
-client = redis.Redis(host='localhost', port= 6379)
-   
+client = redis.Redis(host='192.168.99.125', port= 6379)
 
-async def index(request):   
-    content = open(os.path.join("templates/index.html"), "r").read()    
+
+async def index(request):
+    content = open(os.path.join("templates/index.html"), "r").read()
     return  web.Response(content_type="text/html",text=content)
-    
+
 async def javascript_main(request):
     content = open(os.path.join("main.js"), "r").read()
     return web.Response(content_type="application/javascript", text=content)
@@ -80,10 +80,6 @@ async def javascript_jeelizFaceFilter(request):
 async def Canvas2DDisplay(request):
     content = open(os.path.join("helpers/Canvas2DDisplay.js"), "r").read()
     return web.Response(content_type="application/javascript", text=content)
-
-async def neuralNetsNNC(request):
-    content = open(os.path.join("neuralNets/NNC.json"), "r").read()
-    return web.Response(content_type="application/json", text=content)
 
 async def neuralNets(request):
     content = open(os.path.join("neuralNets/NN_DEFAULT.json"), "r").read()
@@ -106,16 +102,15 @@ async def NNCveryLight(request):
     return web.Response(content_type="application/json", text=content)
 
 async def model_puploc(request):
-    content = open(os.path.join("model/puploc.bin"), "rb").read()
-    
-    return web.Response(content_type="application/octet-stream", body=content)
+    content = open(os.path.join("model/puploc.bin"), "r").read()
+    return web.Response(content_type="application/octet-stream", text=content)
 
 async def estado(request):
-    params = await request.post()    
-    rutaHaar = 'vision/embeddings/'+ str(params['id_usuario']) 
-    
+    params = await request.post()
+    rutaHaar = 'vision/embeddings/'+ str(params['id_usuario'])
+
     if os.path.exists(rutaHaar+'/'+str(params['id_usuario'])+'.npy'):
-        #await ws.send_str("{'accion':'entrenado'}")    
+        #await ws.send_str("{'accion':'entrenado'}")
         mensaje = {'accion':'iniciando','status':'1'}
     else:
         mensaje = {'accion':'iniciando','status':'0'}
@@ -125,16 +120,16 @@ async def estado(request):
         text=json.dumps(
             mensaje
         ),
-    ) 
-   
-async def entrenar(request):     
+    )
+
+async def entrenar(request):
     formData = await request.post()
     # import ipdb; ipdb.set_trace();
     id_usuario = str(formData['id_usuario'])
-    rutaHaar = 'vision/embeddings/'+ id_usuario 
+    rutaHaar = 'vision/embeddings/'+ id_usuario
     frame = formData['data'].split(';base64,')
     image = Image.open(BytesIO(base64.b64decode(frame[1])))
-    #image.save('accept.png', 'PNG')     
+    #image.save('accept.png', 'PNG')
     image = cv2.cvtColor(np.float32(image), cv2.COLOR_BGR2RGB)
     faces,_ = get_faces_live(
         img = image,
@@ -142,7 +137,7 @@ async def entrenar(request):
         rnet = rnet,
         onet = onet,
         image_size = image_size
-    )     
+    )
     if len(faces) == 1 :
         embedding = forward_pass(
             img= faces[0],
@@ -151,72 +146,22 @@ async def entrenar(request):
             embeddings=embeddings,
             phase_train_placeholder=phase_train_placeholder,
             image_size=image_size
-        )      
-        filename = id_usuario        
+        )
+        filename = id_usuario
         save_embedding(
             embedding=embedding,
             filename=filename,
             embeddings_path=rutaHaar
-        )   
+        )
         mensaje = {'accion':'entrenado','razon':'Entrenado'}
     elif len(faces) > 1:
         mensaje = {'accion':'no_entrenado','razon':'multiples rostros detectados'}
         print('multiples rostros detectados')
     else:
         mensaje = {'accion':'no_entrenado','razon':'No se detecto rostro'}
-        print('ningun rostro detectado') 
-    
-
-    return web.Response(
-        content_type="application/json",
-        text=json.dumps(
-            mensaje
-        ),
-    )      
-
-async def validar(request):
-    formData = await request.post()
-    id_usuario = str(formData['id_usuario'])     
-    frame = formData['data'].split(';base64,')
-    image = Image.open(BytesIO(base64.b64decode(frame[1])))  
-    image = cv2.cvtColor(np.float32(image), cv2.COLOR_BGR2RGB)
-    faces,_ = get_faces_live(
-        img = image,
-        pnet = pnet,
-        rnet = rnet,
-        onet = onet,
-        image_size = image_size
-    )     
-    embedding_dict = load_embeddings(formData['id_usuario'])                                   
-    if len(faces)==1:                                                                                                                                  
-        face_embedding = forward_pass(
-            img = faces[0],
-            session= facenet_persistent_session,
-            images_placeholder=images_placeholder,
-            embeddings=embeddings,
-            phase_train_placeholder=phase_train_placeholder,
-            image_size=image_size
-        )                            
-                              
-        _, status = identify_face(
-            embedding=face_embedding,
-            embedding_dict=embedding_dict
-        )                                            
-                                                 
-        if status == True:               
-            mensaje = {'accion':'1','razon':'Identificado'}                                          
-            print('identificado')
-        else:
-            mensaje = {'accion':'0','razon':'No identificado'}                                             
-            print('No identificado')  
-         
-    elif len(faces) > 1:
-        mensaje = {'accion':'No identificado','razon':'Multiples rostros detectados'}                                             
-        print('multiples rostros detectados')
-    else:
-        mensaje = {'accion':'No identificado','razon':'Ningún rostro detectado'}                                             
         print('ningun rostro detectado')
-    
+
+
     return web.Response(
         content_type="application/json",
         text=json.dumps(
@@ -224,49 +169,100 @@ async def validar(request):
         ),
     )
 
-async def inicio_asistencia(request):    
-    params = await request.post()        
-    seg_tick = params['seg_tick']    
-    datetime_inicio =datetime.strptime(params['datetime_inicio'], "%Y-%m-%d %H:%M:%S")     
-    datetime_fin = datetime.strptime(params['datetime_fin'], "%Y-%m-%d %H:%M:%S")     
-    diferenciaHora = datetime_fin.hour - datetime_inicio.hour    
-    diferenciaMin = datetime_fin.minute - datetime_inicio.minute   
-    segundosClase = (diferenciaHora * 60 + diferenciaMin) * 60   
+async def validar(request):
+    formData = await request.post()
+    id_usuario = str(formData['id_usuario'])
+    frame = formData['data'].split(';base64,')
+    image = Image.open(BytesIO(base64.b64decode(frame[1])))
+    image = cv2.cvtColor(np.float32(image), cv2.COLOR_BGR2RGB)
+    faces,_ = get_faces_live(
+        img = image,
+        pnet = pnet,
+        rnet = rnet,
+        onet = onet,
+        image_size = image_size
+    )
+    embedding_dict = load_embeddings(formData['id_usuario'])
+    if len(faces)==1:
+        face_embedding = forward_pass(
+            img = faces[0],
+            session= facenet_persistent_session,
+            images_placeholder=images_placeholder,
+            embeddings=embeddings,
+            phase_train_placeholder=phase_train_placeholder,
+            image_size=image_size
+        )
+
+        _, status = identify_face(
+            embedding=face_embedding,
+            embedding_dict=embedding_dict
+        )
+
+        if status == True:
+            mensaje = {'accion':'1','razon':'Identificado'}
+            print('identificado')
+        else:
+            mensaje = {'accion':'0','razon':'No identificado'}
+            print('No identificado')
+
+    elif len(faces) > 1:
+        mensaje = {'accion':'No identificado','razon':'Multiples rostros detectados'}
+        print('multiples rostros detectados')
+    else:
+        mensaje = {'accion':'No identificado','razon':'Ningún rostro detectado'}
+        print('ningun rostro detectado')
+
+    return web.Response(
+        content_type="application/json",
+        text=json.dumps(
+            mensaje
+        ),
+    )
+
+async def inicio_asistencia(request):
+    formData = await request.post()
+    seg_tick = formData['seg_tick']
+    datetime_inicio =datetime.strptime(formData['datetime_inicio'], "%Y-%m-%d %H:%M:%S")
+    datetime_fin = datetime.strptime(formData['datetime_fin'], "%Y-%m-%d %H:%M:%S")
+    diferenciaHora = datetime_fin.hour - datetime_inicio.hour
+    diferenciaMin = datetime_fin.minute - datetime_inicio.minute
+    segundosClase = (diferenciaHora * 60 + diferenciaMin) * 60
     total_muestas_asistencia =  int(segundosClase/int(seg_tick))
-    room = '{"total_muestras":"'+str(total_muestas_asistencia)+'" }'  
-    client.hset( str(params['id_room']), "data", room)
+    room = '{"total_muestras":"'+str(total_muestas_asistencia)+'" }'
+    client.hset( str(formData['id_room']), "data", room)
     #estudiantes = params['lista_estudiantes']
     estudiantes = ["69","70","71"]
-    client.rpush("lista_"+params['id_room'], *estudiantes )
+    client.rpush("lista_"+formData['id_room'], *estudiantes )
     return(web.Response(
         content_type="application/json",
         text=json.dumps(
-            total_muestas_asistencia                      
-        ),       
+            total_muestas_asistencia
+        ),
     ))
 
-async def terminar_asistencia(request): 
-    global lista_porcentajes   
+async def terminar_asistencia(request):
+    global lista_porcentajes
     params = await request.post()
-    client.delete(params['id_room'])   
+    client.delete(params['id_room'])
     #lista_estudiantes = client.lrange("lista_"+params['id_room'], 0, -1)
     for estudiante in client.lrange("lista_"+params['id_room'], 0, -1):
         client.delete("muestras"+estudiante.decode('utf-8'))
     client.delete("lista_"+params['id_room'])
-    #client.delete("muestras"+params['id_usuario']) 
+    #client.delete("muestras"+params['id_usuario'])
     lista_porcentajes = {}
-    
+
     mensaje = "llaves eliminadas con exito"
     return(web.Response(
         content_type="application/json",
         text=json.dumps(
-            mensaje            
-        ),       
+            mensaje
+        ),
     ))
 
-async def muestra_asistencia(request):    
-    params = await request.post()
-    frame = params['data'].split(';base64,')    
+async def muestra_asistencia(request):
+    print('fdddfdasdedfgferglnerfiernfergberkj')
+    formData = await request.post()
+    frame = formData['data'].split(';base64,')
     image = Image.open(BytesIO(base64.b64decode(frame[1])))
     image = cv2.cvtColor(np.float32(image), cv2.COLOR_BGR2RGB)
     faces,_ = get_faces_live(
@@ -276,7 +272,7 @@ async def muestra_asistencia(request):
         onet=onet,
         image_size=image_size
     )
-    embedding_dict = load_embeddings(params['id_usuario'])
+    embedding_dict = load_embeddings(formData['id_usuario'])
     for face in faces:
         face_embedding = forward_pass(
             img = face,
@@ -289,64 +285,64 @@ async def muestra_asistencia(request):
         _, status = identify_face(
             embedding=face_embedding,
             embedding_dict=embedding_dict
-        )        
-        if status == True:                        
-            lista_asistencia = client.lrange("lista_"+params['id_room'], 0, -1) 
+        )
+        if status == True:
+            lista_asistencia = client.lrange("lista_"+formData['id_room'], 0, -1)
             print(">lista asistencia",lista_asistencia)
             for estudiante in lista_asistencia:
-                if estudiante.decode("utf-8").lower() == params['id_usuario'].lower():
-                    try:                        
-                        contador_asistencia = client.get("muestras"+params['id_usuario'])
+                if estudiante.decode("utf-8").lower() == formData['id_usuario'].lower():
+                    try:
+                        contador_asistencia = client.get("muestras"+formData['id_usuario'])
                         contador_asistencia = int(contador_asistencia.decode("utf-8"))
                         print("contador antes del if:",contador_asistencia)
                     except:
                         pass
-                    if not contador_asistencia:                                                
-                        client.set("muestras"+params['id_usuario'], 1)                                
-                    else:                    
-                        total_muestras = client.hget(params['id_room'], 'data').decode('utf-8')                        
+                    if not contador_asistencia:
+                        client.set("muestras"+formData['id_usuario'], 1)
+                    else:
+                        total_muestras = client.hget(formData['id_room'], 'data').decode('utf-8')
                         total_muestras = json.loads(total_muestras)
-                        if contador_asistencia < int(total_muestras['total_muestras']):                        
+                        if contador_asistencia < int(total_muestras['total_muestras']):
                             contador_asistencia+=1
-                            client.set("muestras"+params['id_usuario'],contador_asistencia)
-                            print("contador despues del if:",contador_asistencia)                     
+                            client.set("muestras"+formData['id_usuario'],contador_asistencia)
+                            print("contador despues del if:",contador_asistencia)
                 else:
-                    print("no entro en la comparacion") 
+                    print("no entro en la comparacion")
         else:
             print("no identificado")
-        
+
         return(web.Response(
         content_type="application/json",
         text=json.dumps(
-            contador_asistencia             
-        ),       
+            contador_asistencia
+        ),
     ))
 
 async def reporte_asistencia(request):
     params = await request.post()
     global lista_porcentajes
     global porcentaje_asistencia
-    total_muestras = client.hget(params['id_room'], 'data').decode('utf-8')                        
+    total_muestras = client.hget(params['id_room'], 'data').decode('utf-8')
     total_muestras = json.loads(total_muestras)
-    lista_estudiantes = client.lrange("lista_"+params['id_room'],0,-1)  
-    
-    for estudiante in lista_estudiantes:    
+    lista_estudiantes = client.lrange("lista_"+params['id_room'],0,-1)
+
+    for estudiante in lista_estudiantes:
         try:
             contador_asistencia = client.get("muestras"+estudiante.decode('utf-8'))
             contador_asistencia = int(contador_asistencia.decode("utf-8"))
-            porcentaje_asistencia = contador_asistencia * 100 / int(total_muestras['total_muestras'])  
+            porcentaje_asistencia = contador_asistencia * 100 / int(total_muestras['total_muestras'])
         except:
             pass
-        if not contador_asistencia:                                                
+        if not contador_asistencia:
             mensaje = "El porcentaje de sistencia es: 0%"
-        else:         
+        else:
             mensaje = "El porcentaje de sistencia es: "+str(int(porcentaje_asistencia))+"%"
 
         lista_porcentajes[estudiante.decode('utf-8')] = porcentaje_asistencia
         mensaje = ''
         porcentaje_asistencia = 0
     reporte = {'curso':params['id_room'],'asistencia':lista_porcentajes}
-    
+
     return web.Response(
         content_type="application/json",
         text=json.dumps(
